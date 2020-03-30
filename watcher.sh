200e07dd2420da915c338a6388edaff36864ebbb
FliiFe <theophile.cailliau@gmail.com>
2020-03-30 22:39:32 +0200

    hook after action, in case hook crashes

--- watcher.sh
+++ watcher.sh
@@ -48,8 +48,8 @@ function fetchurl {
 
     if ! diff "$tempfiledest" "$mostrecent" -q >/dev/null; then
         log $(color "new version" "1;4") $(color "$nick" 92)
-        ../newhook.sh "$nick" &
         cp "$tempfiledest" "$nick-$(date +%s).html"
+        ../newhook.sh "$nick" &
     fi
 }
 
